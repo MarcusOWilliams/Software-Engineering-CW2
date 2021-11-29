425d98ccd91d75c4ef518dfb7918760c6e6efaea
mattfleischer <61191689+mattfle@users.noreply.github.com>
2021-11-29 20:55:37 +0000

    made the background longer so it scrolls for longer

--- SE-CW2-UnityProject/Assets/Scenes/Scripts/background.cs
+++ SE-CW2-UnityProject/Assets/Scenes/Scripts/background.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class background : MonoBehaviour
 {
-    public Transform bgo;
+    public Transform bgo;//background original
+   
     void Start()
     {
-        Instantiate(bgo, new Vector3(0, 3f, 5f), Quaternion.Euler(-270,-180,0));
+
+        backg();
 
     }
-    
 
+    void backg()
+    {
+        //IEnumerator
+        //yield return new WaitForSeconds(1.0f);
+
+        Instantiate(bgo, new Vector3(5f, 3f, 15f), Quaternion.Euler(-270, -180, 0));
+        Instantiate(bgo, new Vector3(30f, 3f, 15f), Quaternion.Euler(-270, -180, 0));
+        Instantiate(bgo, new Vector3(55f, 3f, 15f), Quaternion.Euler(-270, -180, 0));
+    }
+    
 }
