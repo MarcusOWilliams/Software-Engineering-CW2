85ee2df5ed8cff2440904c80dcd7c51580c47058
leojgrant <66557864+leojgrant@users.noreply.github.com>
2021-12-02 00:00:31 +0000

    MainGameDesignChanges
    Trying this commit to revert back to previous state before error

--- SE-CW2-UnityProject/Assets/Scenes/Scripts/background.cs
+++ SE-CW2-UnityProject/Assets/Scenes/Scripts/background.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class background : MonoBehaviour
 {
-    public Transform bgo;//background original
-   
+    //public Transform bgo;//background original
+    [SerializeField] GameObject cameraTriggerCollider;
+    [SerializeField] GameObject gameBackground;
+    [SerializeField] GameObject mainCamera;
+    [SerializeField] GameObject collideWithBackground;
     void Start()
     {
 
@@ -18,9 +21,20 @@ public class background : MonoBehaviour
         //IEnumerator
         //yield return new WaitForSeconds(1.0f);
 
-        Instantiate(bgo, new Vector3(5f, 3f, 15f), Quaternion.Euler(-270, -180, 0));
-        Instantiate(bgo, new Vector3(30f, 3f, 15f), Quaternion.Euler(-270, -180, 0));
-        Instantiate(bgo, new Vector3(55f, 3f, 15f), Quaternion.Euler(-270, -180, 0));
+        //Instantiate(bgo, new Vector3(5f, 3f, 15f), Quaternion.Euler(-270, -180, 0));
+        //Instantiate(bgo, new Vector3(30f, 3f, 15f), Quaternion.Euler(-270, -180, 0));
+        //Instantiate(bgo, new Vector3(55f, 3f, 15f), Quaternion.Euler(-270, -180, 0));
     }
-    
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        Debug.Log("cam triggered");
+        Vector3 background_Pos = gameObject.transform.position;
+        GameObject newGamebackground = Instantiate(gameBackground, new Vector3(background_Pos.x + 99f, background_Pos.y, background_Pos.z), transform.rotation);
+        yield return new WaitForSeconds(10);
+        Destroy(gameObject);
+        
+
+    }
+
 }
