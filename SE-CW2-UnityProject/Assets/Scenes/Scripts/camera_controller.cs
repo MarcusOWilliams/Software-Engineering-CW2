202495ba68db62e0f688c5821cafecb6ade30f33
mattfleischer <61191689+mattfle@users.noreply.github.com>
2021-12-06 11:50:40 +0000

    game slowly increases in speed

--- SE-CW2-UnityProject/Assets/Scenes/Scripts/camera_controller.cs
+++ SE-CW2-UnityProject/Assets/Scenes/Scripts/camera_controller.cs
@@ -11,14 +11,15 @@ public class camera_controller : MonoBehaviour
     [SerializeField] GameObject gameStateObject;
 
     // This float is proportional to the cameras movement speed.
-    public float camera_speed = 5f;
+    private float camera_speed = 1f;
 
     // Update is called once per frame
     void Update()
     {
+        camera_speed = camera_speed + .0001f;
         // First we access the game_state via the object gameStateObject, that holds the game_state string.
         string game_state = gameStateObject.GetComponent<game_state_controller>().game_state;
-
+        
         // If the game_state = "play" we want the camera to move continously.
         if (game_state == "play"){
             main_Camera.transform.Translate(camera_speed * Time.deltaTime, 0, 0);
