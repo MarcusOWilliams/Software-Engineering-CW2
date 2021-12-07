70b55e8d91c39f811647dc8d283ff846b3d1b998
MarcusOWilliams <molowilliams@gmail.com>
2021-12-07 19:50:02 +0000

    Fix text in different resolutions

--- SE-CW2-UnityProject/Assets/Scenes/Scripts/QuitManager.cs
+++ SE-CW2-UnityProject/Assets/Scenes/Scripts/QuitManager.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class QuitManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    private void Start()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
     public void Exit()
     {
         Application.Quit();
