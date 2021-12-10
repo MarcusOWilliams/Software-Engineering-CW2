98978d83d9da8347d72458d8c87a82e6a8cf8d43
MarcusOWilliams <molowilliams@gmail.com>
2021-12-10 16:21:21 +0000

    Added High score

--- SE-CW2-UnityProject/Assets/Scripts/score.cs
+++ SE-CW2-UnityProject/Assets/Scripts/score.cs
@@ -6,6 +6,7 @@ public class score : MonoBehaviour
     [SerializeField] GameObject gameStateObject;
     [SerializeField] Transform character;
     [SerializeField] Text scoreText;
+
     float score_Value;
     float total_Score;
     public static int number_Of_Coins;
@@ -17,17 +18,29 @@ public class score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //update the score to the furthest distance travelled by the character
+        //score will not decrease if moving backwards
+        score_Value = Mathf.Max(score_Value, character.position.x);
+
+        //add the bonus from any coins collected
+        total_Score = score_Value + (number_Of_Coins * 50);
+
         if (gameStateObject.GetComponent<game_state_controller>().game_State == "play")
         {
-            //update the score to the furthest distance travelled by the character
-            //score will not decrease if moving backwards
-            score_Value = Mathf.Max(score_Value, character.position.x);
-
-            //add the bonus from any coins collected
-            total_Score = score_Value + (number_Of_Coins * 50);
 
             //update the score text
             scoreText.text = $"Score: {total_Score.ToString("0")}";
+        }
+
+        else
+        {
+            int HS = menu_UI_Controller.highscore;
+            if ((int) total_Score > HS)
+            { 
+                menu_UI_Controller.highscore = (int) total_Score;
+                
+            }
+            
         }
 
     }
