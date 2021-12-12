ba0d0429d6d198b026e6101c217434600d768f98
MarcusOWilliams <molowilliams@gmail.com>
2021-12-12 14:55:34 +0000

    Update backstory_UI_Controller.cs

--- SE-CW2-UnityProject/Assets/Scripts/backstory_UI_Controller.cs
+++ SE-CW2-UnityProject/Assets/Scripts/backstory_UI_Controller.cs
@@ -6,11 +6,6 @@ using UnityEngine.UI;
 
 public class backstory_UI_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     public void OnExitButtonClicked()
